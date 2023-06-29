d7d7df664562a2fc6a892a1dc3f532b232d0ee17
nulldev <contact@andybao.me>
2023-06-29 18:55:45 -0400

    float-ord support (v0.2.0)

--- src/core/range.rs
+++ src/core/range.rs
@@ -1,7 +1,8 @@
 use std::ops::Range;
 
 /// Returns a range containing the minimum and maximum values in the iterator
-pub fn iter_range<T: Ord + Clone>(mut iter: impl Iterator<Item = T>) -> Option<Range<T>> {
+pub fn iter_range<T: Ord + Clone>(iter: impl IntoIterator<Item = T>) -> Option<Range<T>> {
+    let mut iter = iter.into_iter();
     let Some(mut range) = iter.next().map(|e| Range {
         start: e.clone(),
         end: e
@@ -19,7 +20,8 @@ pub fn iter_range<T: Ord + Clone>(mut iter: impl Iterator<Item = T>) -> Option<R
 }
 
 /// Returns the minimum range required to contain all the input ranges
-pub fn iter_combine_ranges<T: Ord>(mut iter: impl Iterator<Item = Range<T>>) -> Option<Range<T>> {
+pub fn iter_combine_ranges<T: Ord>(iter: impl IntoIterator<Item = Range<T>>) -> Option<Range<T>> {
+    let mut iter = iter.into_iter();
     let Some(mut merged_range) = iter.next() else {
         return None;
     };
@@ -33,3 +35,83 @@ pub fn iter_combine_ranges<T: Ord>(mut iter: impl Iterator<Item = Range<T>>) -> 
     }
     Some(merged_range)
 }
+
+pub trait FloatRangeExt<T> {
+    #[cfg(feature = "float-ord")]
+    fn float_ord(&self) -> Range<float_ord::FloatOrd<T>>;
+}
+
+macro_rules! impl_float_range_ext {
+    ($typ:ty) => {
+        impl FloatRangeExt<$typ> for Range<$typ> {
+            #[cfg(feature = "float-ord")]
+            fn float_ord(&self) -> Range<float_ord::FloatOrd<$typ>> {
+                Range {
+                    start: float_ord::FloatOrd(self.start),
+                    end: float_ord::FloatOrd(self.end)
+                }
+            }
+        }
+    }
+}
+impl_float_range_ext!(f32);
+impl_float_range_ext!(f64);
+
+#[cfg(feature = "float-ord")]
+pub mod float_ord {
+    pub(super) use float_ord::FloatOrd;
+    use std::ops::Range;
+    use super::FloatRangeExt;
+
+    pub trait RangeFloatOrdExt<T: Copy> {
+        fn float_range(self) -> Range<T>;
+    }
+    impl <T: Copy> RangeFloatOrdExt<T> for Range<FloatOrd<T>> {
+        fn float_range(self) -> Range<T> {
+            Range {
+                start: self.start.0,
+                end: self.end.0
+            }
+        }
+    }
+
+    macro_rules! impl_iter_range_float_ord {
+        ($iter_range_name:ident, $iter_combine_ranges_name:ident, $typ:ty) => {
+            /// See: [super::iter_range]
+            pub fn $iter_range_name<T: Into<$typ>>(iter: impl IntoIterator<Item = T>) -> Option<Range<$typ>> {
+                super::iter_range(iter.into_iter().map(|f| f.into()).map(FloatOrd))
+                    .map(RangeFloatOrdExt::float_range)
+            }
+
+            /// See: [super::iter_combine_ranges]
+            pub fn $iter_combine_ranges_name(iter: impl IntoIterator<Item = Range<$typ>>) -> Option<Range<$typ>> {
+                super::iter_combine_ranges(iter.into_iter().map(|f| f.float_ord()))
+                    .map(RangeFloatOrdExt::float_range)
+            }
+        }
+    }
+
+    impl_iter_range_float_ord!(iter_range_float_ord_f32, iter_combine_ranges_float_ord_f32, f32);
+    impl_iter_range_float_ord!(iter_range_float_ord_f64, iter_combine_ranges_float_ord_f64, f64);
+
+    #[cfg(test)]
+    mod tests {
+        use crate::core::range::float_ord::iter_range_float_ord_f32;
+
+        #[test]
+        fn test_iter_range_float_ord_f32() {
+            assert_eq!(iter_range_float_ord_f32(Vec::<f32>::new()), None);
+            assert_eq!(iter_range_float_ord_f32(vec![
+                0.0,
+                1.0,
+                2.0
+            ]), Some(0.0..2.0));
+            assert_eq!(iter_range_float_ord_f32(vec![
+                -5.4,
+                2.3
+            ]), Some(-5.4..2.3));
+            assert_eq!(iter_range_float_ord_f32(vec![1.0]), Some(1.0..1.0));
+        }
+    }
+}
+
